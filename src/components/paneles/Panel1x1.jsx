bce5442ff0197a56154f7a42e7a04d13988286eb
kevinLeonadoGamboa <kevingamboamosquera@gmail.com>
2025-09-11 20:03:38 -0500

    terminacion de Home

---
+++ src/components/paneles/Panel1x1.jsx
@@ -0,0 +1,14 @@
+import "./panel1x1.css"
+export default function Panel1x1(){
+
+    return(
+
+        <div className="panel1x1">
+            
+            <img src="../src/assets/imagenes/foto1x1.webp" alt="" />
+
+        </div>
+
+    )
+
+}
