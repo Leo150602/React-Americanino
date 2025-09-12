c2d8e9dad762b2c3ba4f0db071e1e7f3b8cdd4df
Sebastian Arboleda <juansebastian_gomez456@hotmail.com>
2025-09-11 23:16:44 -0500

    Se le asignan estilos a los detalles del producto

--- src/components/contenidoDetallesProducto/DescripProductos.jsx
+++ src/components/contenidoDetallesProducto/DescripProductos.jsx
@@ -1,30 +1,10 @@
-import { useState } from "react";
-import Swal from "sweetalert2";
 import "./DescripProductos.css"
+import BotonCompra from "./BotonCompra"
+import { useState } from "react";
 
 function DescripProducto({ producto }) {
     const info = producto?.info || {};
     const [seleccionTalla, setSeleccionTalla] = useState(null);
-
-    const agregarCarrito = () => {
-        if (!seleccionTalla) {
-            return Swal.fire("Por favor selecciona una talla antes de agregar al carrito.");
-        }
-
-        let productosGuardados = JSON.parse(localStorage.getItem("productos")) || [];
-        const indexExistente = productosGuardados.findIndex(
-            (p) => p.id === producto.id && p.talla === seleccionTalla
-        );
-
-        if (indexExistente !== -1) {
-            productosGuardados[indexExistente].cantidad += 1;
-        } else {
-            productosGuardados.push({ id: producto.id, talla: seleccionTalla, cantidad: 1 });
-        }
-
-        localStorage.setItem("productos", JSON.stringify(productosGuardados));
-        window.dispatchEvent(new CustomEvent("productoAgregado"));
-    };
     
     return (
         <div className="infoProducto">
@@ -52,9 +32,7 @@ function DescripProducto({ producto }) {
                 )}
             </div>
 
-            <button id="botonCompra" onClick={agregarCarrito}>
-                Agregar al carrito
-            </button>
+            <BotonCompra />
 
             <p className="descripcionProducto">{info.descripcion}</p>
             <p className="paisFabricacion">{info.paisFabricacion}</p>
